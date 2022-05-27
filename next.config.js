c029b72edd64270bd524c4648c7d1df7df688d0b
star <aneb218@gmail.com>
2022-05-27 17:39:49 +0900

    Add github actions

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix:
+      process.env.NODE_ENV === 'production'
+          ? 'https://aneunbyul.github.io/'
+          : '',
+};
