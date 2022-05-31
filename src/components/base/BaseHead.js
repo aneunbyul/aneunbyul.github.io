dde745c8342e7ae0dfdf5ac68d12d54460a24788
star <aneb218@gmail.com>
2022-05-31 17:43:48 +0900

    baseHead changed

--- src/components/base/BaseHead.js
+++ src/components/base/BaseHead.js
@@ -6,8 +6,8 @@ const BaseHead = ({title = 'TelePIX', children}) => {
   return (
     <NoneDiv>
       <Head>
-        <title>{`TelePIX ERP : ${title.toUpperCase()}`}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{`AnEunByul : ${title.toUpperCase()}`}</title>
+        {/*<link rel="icon" href="/favicon.ico" />*/}
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
